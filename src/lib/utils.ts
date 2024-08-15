3a83c0630e021097fe54429a8393cad5d51e2aeb
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2024-08-15 11:49:23 +0100

    chore: Update tsconfig.json to use CommonJS module format

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]): string {
   const uniqueClasses = Array.from(new Set(clsx(inputs).split(' ')));
   return twMerge(uniqueClasses.join(' '));
 }
