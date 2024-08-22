c350a5860f9266144b293ca4773f3673248c9415
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2024-08-22 09:36:58 +0100

    Update build configuration for custom icon and executable name

--- forge.config.ts
+++ forge.config.ts
@@ -33,11 +33,14 @@ const config: ForgeConfig = {
   ],
   plugins: [
     new VitePlugin({
-      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-      // If you are familiar with Vite configuration, it will look really familiar.
+      // `build` can specify multiple entry builds, which can be
+      // Main process, Preload scripts, Worker process, etc.
+      // If you are familiar with Vite configuration,
+      // it will look really familiar.
       build: [
         {
-          // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+          // `entry` is just an alias for `build.lib.entry`
+          // in the corresponding file of `config`.
           entry: 'src/main.ts',
           config: 'vite.main.config.ts',
         },
