4b5da1982d06c2492d84d1463718bd9a777ea0c3
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2024-09-14 14:39:45 +0100

    Refactor app.vue to support internationalization

--- src/ui/app.vue
+++ src/ui/app.vue
@@ -3,6 +3,7 @@ import Layout from '@/ui/layouts/DefaultLayout.vue';
 import { VideoConverter, AudioConverter } from '@/ui/blocks';
 import { createConverterStore } from '@/ui/stores';
 import { onMounted, onBeforeUnmount } from 'vue';
+import { useLanguagePreferences } from '@/ui/composables/language-mode';
 
 const useVideoConverterStore = createConverterStore();
 const videoStore = useVideoConverterStore();
@@ -10,9 +11,16 @@ const videoStore = useVideoConverterStore();
 const useAudioConverterStore = createConverterStore();
 const audioStore = useAudioConverterStore();
 
+const {
+  currentLocale,
+  setLocale,
+} = useLanguagePreferences();
+
 onMounted(async () => {
   await videoStore.init();
   await audioStore.init();
+
+  setLocale(currentLocale.value);
 });
 
 onBeforeUnmount(() => {
